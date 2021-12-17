182589db6bf482fc5a3dd7a0c284a3e7ae5ba6ff
gulnar8 <jmargul8@gmail.com>
2021-12-16 21:45:48 -0500

    thankgiving

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -2,6 +2,6 @@ package day1;
 
 public class StringPractice {
     public static void main(String[] args) {
-        System.out.println("Happy Ester");
+        System.out.println("Happy thanksgiving");
     }
 }
